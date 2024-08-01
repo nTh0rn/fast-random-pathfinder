cd5674dbffb8e36ceea3e51343c5f623e3f36681
nThorn <30553301+nTh0rn@users.noreply.github.com>
2024-07-31 22:30:18 -0400

    First CMake build

---
+++ CMakeFiles/CraPath.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CraPath.dir/CraPath.cpp.obj"
+  "CMakeFiles/CraPath.dir/CraPath.cpp.obj.d"
+  "CraPath.exe"
+  "CraPath.exe.manifest"
+  "CraPath.lib"
+  "CraPath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CraPath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CraPath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector <char> 
+
+int main(int argc, char *argv[])
+{
+   cout << "Hello CMake!" << std::endl;
+   return 0;
+}
